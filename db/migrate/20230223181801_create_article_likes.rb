534601002c684a2d5b7c8761fda348cebbd6105b
k_akihiro1 <k.shuhari@gmail.com>
2023-02-25 20:12:09 +0900

    モデルの再作成

--- db/migrate/20230223181801_create_article_likes.rb
+++ db/migrate/20230223181801_create_article_likes.rb
@@ -2,7 +2,7 @@ class CreateArticleLikes < ActiveRecord::Migration[6.0]
   def change
     create_table :article_likes do |t|
       t.references :user, null: false, foreign_key: true
-      t.reference :article
+      t.references :article
 
       t.timestamps
     end
