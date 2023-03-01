2e8ff57c8508c1a7c71fd4077a8add43a803ab26
k_akihiro1 <k.shuhari@gmail.com>
2023-03-02 00:41:07 +0900

    ささやかな修正

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,6 @@
 module Api::V1
   class ArticlesController < BaseApiController
-    # http://localhost:3000//api/v1/articles(.:format)
+    # http://localhost:3000/api/v1/articles(.:format)
     def index
       articles = Article.order('created_at desc')
       render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
