7ccfcb99e8f6b7b86626c9bc8eaceea620f846fb
k_akihiro1 <k.shuhari@gmail.com>
2023-03-17 00:53:36 +0900

    新規登録 API の実装

---
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+
+  private
+    #ユーザー登録時に使用
+    def sign_up_params
+      binding.pry
+      params.require(:registration).permit(:name, :email, :password,:password_confirmation)
+    end
+
+    #ユーザー更新時に使用
+    def account_update_params
+        params.require(:registration).permit(:name, :email)
+    end
+end
