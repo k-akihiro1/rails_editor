2150a369a6e6136089f286c8344e105faffdb20a
k_akihiro1 <k.shuhari@gmail.com>
2023-03-18 17:30:59 +0900

    passowrdが違う時、ログインできないことを確認

--- spec/requests/api/v1/auth/sessions_spec.rb
+++ spec/requests/api/v1/auth/sessions_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe "Api::V1::Auth::Sessions", type: :request do
     context "email が一致しないとき" do
       let(:user){ create(:user)}
       let(:params) {attributes_for(:user,email: "hogehoge", paasword: user.password) }
-      fit "エラーが起きて登録できない" do
+      it "エラーが起きて登録できない" do
         subject
         #body情報
         res = JSON.parse(response.body)
@@ -40,12 +40,25 @@ RSpec.describe "Api::V1::Auth::Sessions", type: :request do
       end
     end
     context "password が存在しないとき" do
-      let(:params) {{ registration: attributes_for(:user,password: nil) }}
+      let(:user){ create(:user)}
+      let(:params) {attributes_for(:user,email: user.email, paasword: "hogehoge") }
       it "エラーが起きて登録できない" do
-        expect { subject }.to change { User.count }.by(0)
-        expect(response).to have_http_status(:unprocessable_entity)
-        res=JSON.parse(response.body)
-        expect(res["errors"]["password"]).to eq ["can't be blank"]
+        subject
+        #body情報
+        res = JSON.parse(response.body)
+        #hearder情報
+        header = response.headers
+
+        #bodyのエラーの確認
+        expect(res["errors"]).to include "Invalid login credentials. Please try again."
+
+        #ステータスコードの確認
+        expect(response).to have_http_status(401)
+
+        #heardersの情報がないことを確認
+        expect(response.header["access-token"]).to be_blank
+        expect(response.header["client"]).to be_blank
+        expect(response.header["uid"]).to be_blank
       end
     end
   end
