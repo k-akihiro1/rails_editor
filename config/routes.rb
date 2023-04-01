3dc8ddca23d471c80623c4434f367398e6cbad92
k_akihiro1 <k.shuhari@gmail.com>
2023-04-02 01:26:46 +0900

    下書き一覧テストの実装

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for "User", at: "auth", controllers: {
         registrations: "api/v1/auth/registrations",
       }
-      resources :articles
       namespace :articles do
         resources :drafts, only: [:index, :show]
       end
--- spec/requests/api/v1/articles/drafts_spec.rb
+++ spec/requests/api/v1/articles/drafts_spec.rb
@@ -2,9 +2,26 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Articles::Drafts", type: :request do
   let(:headers) { current_user.create_new_auth_token }
   let(:current_user) { create(:user) }
+
   describe "GET /api/v1/articles/drafts" do
+    subject { get(api_v1_articles_drafts_path, headers: headers) }
     context "自分の書いた下書きの記事が存在するとき" do
-      it "自分の書いた下書きのみが取得できる" do
+      # ログインユーザーの記事
+      let!(:article1) { create(:article, :draft, user: current_user) }
+      # それ以外の記事
+      let!(:article2) { create(:article, :draft) }
+
+      fit "自分の書いた下書きのみが取得できる" do
+        binding.pry
+        subject
+        res = JSON.parse(response.body)
+        # 配列の要素の数を確認
+        expect(res.length).to eq 1
+        expect(res[0]["id"]).to eq article1.id
+        # 記事のKeyが一致しているか確認
+        expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
+        # ユーザーのKeyが一致しているか確認
+        expect(res[0]["user"].keys).to eq ["id", "name", "email"]
       end
     end
   end
